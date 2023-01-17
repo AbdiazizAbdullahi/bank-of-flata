046eae7ae3615d46013c1814c8bc97910a89c8ce
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-01-17 16:27:14 +0300

    updated App.css

--- src/components/filter.js
+++ src/components/filter.js
@@ -9,38 +9,38 @@ function Filter({ transactions, onDelete }) {
   
     return (
       <div className="filter-container">
-        <input
+        <input className='input-s'
           type="text"
           placeholder="Search for transactions"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-/>
-<table className="transactions-table">
-<thead>
-<tr>
-<th>Date</th>
-<th>Category</th>
-<th>Description</th>
-<th>Amount</th>
-<th>Actions</th>
-</tr>
-</thead>
-<tbody>
-{filteredTransactions.map((transaction) => (
-<tr key={transaction.id}>
-<td>{transaction.date}</td>
-<td>{transaction.category}</td>
-<td>{transaction.description}</td>
-<td>{transaction.amount}</td>
-<td>
-<button className='delete-btn' onClick={() => onDelete(transaction.id)}>Delete</button>
-</td>
-</tr>
-))}
-</tbody>
-</table>
-</div>
-);
+        />
+        <table className="transactions-table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Category</th>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredTransactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>{transaction.date}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.amount}</td>
+                <td>
+                  <button className='delete-btn' onClick={() => onDelete(transaction.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+  );
 }
 
 export default Filter;
--- src/components/form.js
+++ src/components/form.js
@@ -7,50 +7,53 @@ function Form({ onSubmit }) {
     const [amount, setAmount] = useState("");
     
     const handleSubmit = (event) => {
-    event.preventDefault();
-    onSubmit({ date, category, description, amount });
-    setDate("");
-    setCategory("");
-    setDescription("");
-    setAmount("");
+      event.preventDefault();
+      onSubmit({ date, category, description, amount });
+      setDate("");
+      setCategory("");
+      setDescription("");
+      setAmount("");
     };
     
     return (
-    <form className="form-container" onSubmit={handleSubmit}>
-    <label>
-    Date:
-    <input
-    type="text"
-    value={date}
-    onChange={(e) => setDate(e.target.value)}
-    />
-    </label>
-    <label>
-    Category:
-    <input
-    type="text"
-    value={category}
-    onChange={(e) => setCategory(e.target.value)}
-    />
-    </label>
-    <label>
-    Description:
-    <input
-    type="text"
-    value={description}
-    onChange={(e) => setDescription(e.target.value)}
-    />
-    </label>
-    <label>
-    Amount:
-    <input
-    type="text"
-    value={amount}
-    onChange={(e) => setAmount(e.target.value)}
-    />
-    </label>
-  <button type="submit">Add Transaction</button>
-</form>
+      <div className="form-div">
+      <form className="form-container" onSubmit={handleSubmit}>
+        <label>
+          Date:
+        <input
+          className="date-input"
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        </label>
+        <label>
+          Category:
+        <input
+          type="text"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        </label>
+        <label>
+          Description:
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        </label>
+        <label>
+          Amount:
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        </label>
+        <button type="submit">Add Transaction</button>
+      </form>
+      </div>
 );
 }
 
